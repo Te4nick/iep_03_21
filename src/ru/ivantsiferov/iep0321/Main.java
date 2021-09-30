aaefbb0d13c5588a8480b801110c51fe0f5136e5
Ivan Antsiferov <ivan.ivantz@gmail.com>
2021-09-30 14:38:15 +0300

    Init repo

---
+++ src/ru/ivantsiferov/iep0321/Main.java
@@ -0,0 +1,9 @@
+package ru.ivantsiferov.iep0321;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
